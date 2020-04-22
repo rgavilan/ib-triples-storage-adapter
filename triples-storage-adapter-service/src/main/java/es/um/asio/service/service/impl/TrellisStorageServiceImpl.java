75f2b063cf3be9e4b3a80b2c2b577ae595d753b5
Alejandro Martino Uria <amartino@izertis.com>
2020-04-22 17:31:23 +0200

    SEMANTMURC-113 create containers

--- triples-storage-adapter-service/src/main/java/es/um/asio/service/service/impl/TrellisStorageServiceImpl.java
+++ triples-storage-adapter-service/src/main/java/es/um/asio/service/service/impl/TrellisStorageServiceImpl.java
@@ -3,6 +3,9 @@ package es.um.asio.service.service.impl;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpStatus;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,7 +39,7 @@ public class TrellisStorageServiceImpl implements TriplesStorageService {
 	private TrellisUtils trellisUtils;
 
 	@Value("${app.trellis.endpoint}")
-	private String trellisUrlEndPoind;
+	private String trellisUrlEndPoint;
 
 	/**
 	 * Process.
@@ -58,32 +61,9 @@ public class TrellisStorageServiceImpl implements TriplesStorageService {
 		}
 	}
 
+	
 	/**
-	 * Save.
-	 *
-	 * @param message the message
-	 */
-	public void save(ManagementBusEvent message) {
-		logger.info("Saving object in trellis : " + message.toString());
-
-		if(StringUtils.isNoneBlank(message.getIdModel())) {
-			Model model = trellisUtils.toObject(message.getModel());
-			
-			Response postResponse = RestAssured.given()
-					.contentType(MediaTypes.TEXT_TURTLE)
-					.header("slug", message.getIdModel())
-					.body(model, new RdfObjectMapper()).post(trellisUrlEndPoind);
-			
-			if (postResponse.getStatusCode() != HttpStatus.SC_CREATED) {
-				logger.error("Error saving the object: " + message.getModel());
-				logger.error("cause: " + postResponse.getBody().asString());
-				throw new RuntimeTrellisException("Error saving in Trellis the object: " + message.getModel());
-			}
-		}
-	}
-
-	/**
-	 * Gets the.
+	 * Gets the Model from resourceUri.
 	 *
 	 * @param resourceUri the resource uri
 	 * @return the model
@@ -97,5 +77,117 @@ public class TrellisStorageServiceImpl implements TriplesStorageService {
 
 		return model;
 
+	}
+	
+	/**
+	 * Save.
+	 *
+	 * @param message the message
+	 */
+	public void save(ManagementBusEvent message) {
+		logger.info("Saving object in trellis : " + message.toString());
+
+		if(StringUtils.isNoneBlank(message.getIdModel())) {
+			if(!existsContainer(message)) {
+				createContainer(message);
+			}
+			
+			// we insert the entry in trellis
+			createEntry(message);
+		}
+	}
+	
+	
+			
+	/**
+	 * Exists container.
+	 *
+	 * @param message the message
+	 * @return true, if successful
+	 */
+	public boolean existsContainer(ManagementBusEvent message) {
+		boolean result = false;
+		
+		// FIXME remove
+		if("${app.trellis.endpoint}".equals(trellisUrlEndPoint)) {
+			trellisUrlEndPoint = "http://localhost:80/";			
+		}
+		
+		String urlContainer =  trellisUrlEndPoint + message.getClassName();
+		Model model;
+		try {
+			model = RestAssured.given()
+					.header("Accept", MediaTypes.TEXT_TURTLE)
+					.expect()
+					.when().get(urlContainer)
+					.as(Model.class, new RdfObjectMapper(urlContainer));
+					result = model.size() > 0 ? true : false;
+		} catch (Exception e) {
+			result = false;
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * Creates the container.
+	 *
+	 * @param message the message
+	 */
+	public void createContainer(ManagementBusEvent message) {
+		logger.info("Creating a container");
+				
+		// FIXME remove
+		if("${app.trellis.endpoint}".equals(trellisUrlEndPoint)) {
+			trellisUrlEndPoint = "http://localhost:80/";			
+		}
+								
+		Model model = ModelFactory.createDefaultModel();
+		model.createProperty("http://hercules.org");
+		Resource resourceProperties = model.createResource();
+		Property a = model.createProperty("http://www.w3.org/ns/ldp#", "a");
+		Property dcterms = model.createProperty("http://purl.org/dc/terms/", "title");
+		
+		resourceProperties.addProperty(a, "Container");
+		resourceProperties.addProperty(a, "BasicContainer");
+		resourceProperties.addProperty(dcterms, message.getClassName() + " Container");
+		
+		Response postResponse;
+		try {
+			postResponse = RestAssured.given()
+					.contentType(MediaTypes.TEXT_TURTLE)
+					.header("slug", message.getClassName())					
+					.header("link", "<http://www.w3.org/ns/ldp#BasicContainer>; rel=\"type\"")
+					.body(model, new RdfObjectMapper())
+					.post(trellisUrlEndPoint);
+			
+			if (postResponse.getStatusCode() != HttpStatus.SC_CREATED) {
+				logger.error("Error creating container cause " + postResponse.getStatusCode());
+			} 
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Creates the entry.
+	 *
+	 * @param message the message
+	 */
+	public void createEntry(ManagementBusEvent message) {
+		Model model = trellisUtils.toObject(message.getModel());
+		String urlContainer =  trellisUrlEndPoint + message.getClassName();
+		
+		Response postResponse = RestAssured.given()
+				.contentType(MediaTypes.TEXT_TURTLE)
+				.header("slug", message.getIdModel())
+				.body(model, new RdfObjectMapper()).post(urlContainer);
+		
+		if (postResponse.getStatusCode() != HttpStatus.SC_CREATED) {
+			logger.error("Error saving the object: " + message.getModel());
+			logger.error("cause: " + postResponse.getBody().asString());
+			throw new RuntimeTrellisException("Error saving in Trellis the object: " + message.getModel());
+		}
 	}
 }
--- triples-storage-adapter-service/src/test/java/es/um/asio/service/test/service/TriplesStorageServiceTest.java
+++ triples-storage-adapter-service/src/test/java/es/um/asio/service/test/service/TriplesStorageServiceTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import es.um.asio.abstractions.domain.ManagementBusEvent;
+import es.um.asio.abstractions.domain.Operation;
 import es.um.asio.service.service.TriplesStorageService;
 import es.um.asio.service.service.impl.TrellisStorageServiceImpl;
 import es.um.asio.service.util.TrellisUtils;
@@ -37,5 +39,24 @@ public class TriplesStorageServiceTest {
     @Test
     public void test_Infraestructure() {
         assertNotNull(service);
+        
+        ManagementBusEvent message = new ManagementBusEvent();
+        
+        String model = "<rdf:RDF\r\n" + "    xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\r\n"
+				+ "    xmlns:j.0=\"http://example.org/\"\r\n"
+				+ "    xmlns:j.1=\"http://www.w3.org/2001/asio-rdf/3.0#\">\r\n"
+				+ "  <j.0:ConceptoGrupoDummy rdf:about=\"http://example.org/E0A6-01/5/DESCRIPTORES/LENGUAJES+PLASTICOS\">\r\n"
+				+ "    <j.1:texto>LENGUAJES PLASTICOS</j.1:texto>\r\n"
+				+ "    <j.1:codTipoConcepto>DESCRIPTORES</j.1:codTipoConcepto>\r\n"
+				+ "    <j.1:numero>5</j.1:numero>\r\n"
+				+ "    <j.1:idGrupoInvestigacion>E0A6-01</j.1:idGrupoInvestigacion>\r\n"
+				+ "  </j.0:ConceptoGrupoDummy>\r\n" + "</rdf:RDF>";
+        
+        message.setModel(model);
+        message.setClassName("ConceptoGrupoDummy");
+        // message.setIdModel("07dcf82c-43ec-40b5-aa21-e41801d3b582");
+        message.setIdModel("10886753");
+        message.setOperation(Operation.INSERT);
+        service.process(message);
     }
 }
