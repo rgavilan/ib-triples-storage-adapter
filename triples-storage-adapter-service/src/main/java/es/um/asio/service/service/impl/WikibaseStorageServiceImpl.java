eae86686eafdd52f7eae3d7df86ba3c9dfcc9937
Alejandro Martino Uria <amartino@izertis.com>
2020-05-06 17:09:39 +0200

    SEMANTMURC-100 testing

--- triples-storage-adapter-service/src/main/java/es/um/asio/service/service/impl/WikibaseStorageServiceImpl.java
+++ triples-storage-adapter-service/src/main/java/es/um/asio/service/service/impl/WikibaseStorageServiceImpl.java
@@ -17,34 +17,40 @@ import es.um.asio.service.service.TriplesStorageService;
 @ConditionalOnProperty(prefix = "app.wikibase", name = "enabled", havingValue = "true", matchIfMissing = true)
 @Service
 public class WikibaseStorageServiceImpl implements TriplesStorageService {
-	
+
 	private final Logger logger = LoggerFactory.getLogger(WikibaseStorageServiceImpl.class);
-	
+
 	@Autowired
 	private WikibaseStorageRepository wikibaseStorageRepository;
-	
+
 	@Override
 	public void process(ManagementBusEvent message) throws TripleStoreException {
 		if (this.logger.isDebugEnabled()) {
-            this.logger.debug("Insert new message: {}", message);
-        }
-/*
-        final JsonObject jMessage = JsonParser.parseString(message).getAsJsonObject();
+			this.logger.debug("Insert new message: {}", message);
+		}
 
-        final String idMessage = jMessage.get("id").getAsString();
-        final String type = jMessage.get("type").getAsString();
-        final JsonObject jData = jMessage.get("data").getAsJsonObject();
+		switch (message.getOperation()) {
+		case INSERT:
+			this.save(message);
+			break;
+		case UPDATE:
+			break;
+		case DELETE:
+			break;
+		default:
+			break;
+		}
+	}
 
-        if (this.logger.isDebugEnabled()) {
-            this.logger.debug("recived TYPE: [{}]\tID: [{}]\tCONTENT: {}", type, idMessage, jData.toString());
-            this.logger.debug("Ready to update wikidata");
-        }
+	void save(ManagementBusEvent message) throws TripleStoreException {
+		if (this.logger.isDebugEnabled()) {
+			this.logger.debug("Wikidata updated");
+		}
 
-        this.wikibaseStorageRepository.save(jData);
-*/
-        if (this.logger.isDebugEnabled()) {
-            this.logger.debug("Wikidata updated");
-        }
+		final JsonObject jMessage = JsonParser.parseString(message.getModel()).getAsJsonObject();
+
+		this.wikibaseStorageRepository.save(jMessage);
+
 	}
 
 }
